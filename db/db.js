f92b0736d4856102c98fd250e8fb9de424754a82
talgat55 <lightx_design@yahoo.com>
2016-10-27 22:28:27 +0600

    create tables and check for exist table in mysql

--- db/db.js
+++ db/db.js
@@ -3,11 +3,11 @@ import config from "./../config/config.json"
 
 if (config.database_type == "postgres") { // Postgress
 
-    let potgress = require("./postgress.js")
+    //  let potgress = require("./postgress.js")
 
 } else if (config.database_type == "mysql") { //   Mysql
 
-    // work //  let mysql = require("./mysql.js")
+    let mysql = require("./mysql.js")
 
 } else if (config.database_type == "mongodb") {
     // work //  let mongo = require("./mongo.js")
--- db/mysql.js
+++ db/mysql.js
@@ -1,14 +1,12 @@
-import config from "./../config/config.json"
+//# Require config options and check connection also create tables
+//import config from "./../config/config.json"
 import mysql from "mysql"
-
+let config;
 var pool = mysql.createPool({
-    host: 'example.org',
-    user: config.userdb ? config.userdb : "root",
-    password: config.passworddb ? config.passworddb : "root",
-    connectionLimit: config.connections ? config.connections : "10",
-    port: config.port ? config.port : "5432",
-    connectTimeout: config.limittimeconnect ? config.limittimeconnect : "30000",
-    database: config.database ? config.database : "my_db"
+    host: 'localhost',
+    user: "root",
+    password: "password",
+    database: "server_node"
 });
 
 pool.getConnection(function(err, connection) {
@@ -16,8 +14,28 @@ pool.getConnection(function(err, connection) {
     if (err) {
         console.error('error connecting: ' + err.stack);
         return;
+    } else {
+        console.log('connected successful as id ' + connection.threadId);
+        connection.query('CREATE TABLE `users` (  `id` INT(11) NOT NULL AUTO_INCREMENT, `name` CHAR(30) NOT NULL, `role` CHAR(30) NOT NULL, PRIMARY KEY(`id`) )', function(err, rows) {
+            if (err) {
+                if (err.code == "ER_TABLE_EXISTS_ERROR") {
+
+                    console.log("EXIST Table ")
+                }else{
+                    console.log(err)
+                }
+                
+            } else {
+
+                console.log("successful query")
+
+            }
+
+            connection.release();
+
+        });
     }
-    console.log('connected as id ' + connection.threadId);
+
     // connected! (unless `err` is set) 
 });
 
